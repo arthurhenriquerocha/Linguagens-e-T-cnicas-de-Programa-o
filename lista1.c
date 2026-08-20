b30b9387fd791462ba0b35de427da88b5dbb00a1
Arthur Henrique da Rocha <131204931+arthurhenriquerocha@users.noreply.github.com>
2026-08-20 10:28:33 -0300

    Change scanf format to accept comma-separated values

--- lista1.c
+++ lista1.c
@@ -164,12 +164,12 @@ int x1, x2, y1, y2, p1, p2;
 float dist;
 
 printf("Insira as cordenadas do ponto P1: ");
-scanf("%d %d", &x1, &y1);
+scanf("%d,%d", &x1, &y1);
 
 
 
 printf("Insira as cordenadas do ponto P2: ");
-scanf("%d %d", &x2, &y2);
+scanf("%d,%d", &x2, &y2);
 
 p1 = pow(x2-x1, 2);
 p2 = pow(y2-y1, 2);
