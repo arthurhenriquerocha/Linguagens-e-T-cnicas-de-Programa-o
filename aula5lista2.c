4cac9220a5c849a8a35b9d907d9f3beaf543faef
Arthur Henrique da Rocha <131204931+arthurhenriquerocha@users.noreply.github.com>
2026-08-24 16:06:33 -0300

    Add commented code for age and birth year calculation
    The code now includes a commented-out section for calculating birth year based on age and current year, alongside the existing logic for finding the largest of four numbers.

--- aula5lista2.c
+++ aula5lista2.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+	/*
+long double idade, ano, anonas;
+	
+	printf("Digite a sua idade atual: ");
+	scanf("%Lf", &idade);
+	printf("Digite o ano em que voce esta: ");
+	scanf("%Lf", &ano);
+	
+	anonas = ano - idade;
+	
+	printf("O ano em que voce nasceu é: %.0Lf", anonas);
+//ex 1
+
+
+
+
+
+
+
+
+
 	
 	int a, b, c, d, maior, maior_temp1, maior_temp2;
 	printf("Digite os valores a serem comparados: ");
@@ -15,9 +36,9 @@ int main(int argc, char *argv[]) {
 	// compara o maior dos tres primeiros e o quarto
 	printf("O maior entre |%d|%d|%d|%d| = |%d|", a, b, c, d, maior);
 	
+	// ex 10
 	
-	
-	
+	*/
 	
 	
 	
