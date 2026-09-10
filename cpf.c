5e2013b904743ef11cc5e211338244283ac9046e
Arthur Henrique da Rocha <131204931+arthurhenriquerocha@users.noreply.github.com>
2026-09-10 10:10:02 -0300

    Update cpf.c

--- cpf.c
+++ cpf.c
@@ -23,39 +23,39 @@ int main(int argc, char *argv[]) {
            dgv1, dgv2);
 
     
-    // CÁLCULO DO 1º DÍGITO VERIFICADOR
+    // CALCULO DO 1º DIGITO VERIFICADOR
     
 
     soma = multidigito(dig1, 10)+multidigito(dig2, 9)+multidigito(dig3, 8)+multidigito(dig4, 7)+multidigito(dig5, 6)+multidigito(dig6, 5)+multidigito(dig7, 4)+multidigito(dig8, 3)+multidigito(dig9, 2);
     soma *= 10;
     resto = soma % 11;
 
-    // Se o resto for 10, o dígito será 0
+    // Se o resto for 10, o digito sera 0
     if (resto == 10) {
         resto = 0;
     }
 
    
-    // CÁLCULO DO 2º DÍGITO VERIFICADOR
+    // CALCULO DO 2º DIGITO VERIFICADOR
     
 
     soma = multidigito(dig1, 11)+multidigito(dig2, 10)+multidigito(dig3, 9)+multidigito(dig4, 8)+multidigito(dig5, 7)+multidigito(dig6, 6)+multidigito(dig7, 5)+multidigito(dig8, 4)+multidigito(dig9, 3)+multidigito(dgv1, 2);
     soma *= 10;
     restoII = soma % 11;
 
-    // Se o resto for 10, o dígito será 0
+    // Se o resto for 10, o digito sera 0
     if (restoII == 10) {
         restoII = 0;
     }
 
     
-    // VALIDAÇÃO
+    // VALIDACAO
     
 
     if (resto == dgv1 && restoII == dgv2) {
-        printf("\n\nCPF Válido!");
+        printf("\n\nCPF Valido!");
     } else {
-        printf("\n\nCPF Inválido!");
+        printf("\n\nCPF Invalido!");
     }
 
     return 0;
